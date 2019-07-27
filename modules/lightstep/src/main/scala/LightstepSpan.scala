f960d49959f9c66dffefe4807a571c5b9908c0cd
Dejan Mijic <dmijic@acm.org>
2019-07-28 01:06:11 +0200

    Copy and rename JaegerSpan to LightstepSpan

--- modules/lightstep/src/main/scala/LightstepSpan.scala
+++ modules/lightstep/src/main/scala/LightstepSpan.scala
@@ -6,16 +6,35 @@ package natchez
 package lightstep
 
 import cats.effect.{ Resource, Sync }
+import cats.implicits._
 import io.{ opentracing => ot }
+import io.opentracing.propagation.{ Format, TextMapAdapter }
+
+import scala.collection.JavaConverters._
 
 private[lightstep] final case class LightstepSpan[F[_]: Sync](
   tracer: ot.Tracer,
   span: ot.Span
 ) extends Span[F] {
+  
+  import TraceValue._
 
-  override def kernel: F[Kernel] = ???
+  override def kernel: F[Kernel] =
+    Sync[F].delay {
+      val m = new java.util.HashMap[String, String]
+      tracer.inject(span.context, Format.Builtin.HTTP_HEADERS, new TextMapAdapter(m))
+      Kernel(m.asScala.toMap)
+    }
 
-  override def put(fields: (String, TraceValue)*): F[Unit] = ???
+  override def put(fields: (String, TraceValue)*): F[Unit] =
+    fields.toList.traverse_ {
+      case (k, StringValue(v))  => Sync[F].delay(span.setTag(k, v))
+      case (k, NumberValue(v))  => Sync[F].delay(span.setTag(k, v))
+      case (k, BooleanValue(v)) => Sync[F].delay(span.setTag(k, v))
+    }
 
-  override def span(name: String): Resource[F,Span[F]] = ???
+  override def span(name: String): Resource[F,Span[F]] =
+    Resource
+      .make(Sync[F].delay(tracer.buildSpan(name).asChildOf(span).start()))(s => Sync[F].delay(s.finish()))
+      .map(LightstepSpan(tracer, _))
 }
