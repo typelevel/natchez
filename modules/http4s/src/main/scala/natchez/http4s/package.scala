0b75667cbe86e2af68d7dd0443e7313ad525f42b
Rob Norris <rob_norris@mac.com>
2021-02-08 16:21:37 -0600

    headers

--- modules/http4s/src/main/scala/natchez/http4s/package.scala
+++ modules/http4s/src/main/scala/natchez/http4s/package.scala
@@ -1,3 +1,7 @@
+// Copyright (c) 2019-2020 by Rob Norris and Contributors
+// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
 package natchez
 
 package object http4s {
