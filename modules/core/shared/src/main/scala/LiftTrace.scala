16e9a90d0af8bf73cd5bfdd906f383a05648c7d3
Arman Bilge <armanbilge@gmail.com>
2021-11-16 23:54:07 +0000

    LiftTrace concept, take 3

--- modules/core/shared/src/main/scala/LiftTrace.scala
+++ modules/core/shared/src/main/scala/LiftTrace.scala
@@ -4,15 +4,30 @@
 
 package natchez
 
+import cats.arrow.FunctionK
 import cats.data.Kleisli
+import cats.effect.IO
+import cats.effect.kernel.MonadCancelThrow
+import cats.~>
 
 trait LiftTrace[F[_], G[_]] {
-  def lift[A](span: Span[F], ga: G[A]): F[A]
+  def run[A](span: Span[F])(f: Trace[G] => G[A]): F[A]
+  def liftK: F ~> G
 }
 
-object LiftTrace {
-  implicit def kleisliInstance[F[_]]: LiftTrace[F, Kleisli[F, Span[F], *]] =
+object LiftTrace extends LiftTraceLowPriority {
+  implicit val ioInstance: LiftTrace[IO, IO] = new LiftTrace[IO, IO] {
+    def run[A](span: Span[IO])(f: Trace[IO] => IO[A]): IO[A] =
+      Trace.ioTrace(span).flatMap(f)
+    def liftK: IO ~> IO = FunctionK.id
+  }
+}
+
+private[natchez] trait LiftTraceLowPriority {
+  implicit def kleisliInstance[F[_]: MonadCancelThrow]: LiftTrace[F, Kleisli[F, Span[F], *]] =
     new LiftTrace[F, Kleisli[F, Span[F], *]] {
-      def lift[A](span: Span[F], ga: Kleisli[F, Span[F], A]): F[A] = ga(span)
+      def run[A](span: Span[F])(f: Trace[Kleisli[F, Span[F], *]] => Kleisli[F, Span[F], A]): F[A] =
+        f(Trace.kleisliInstance).run(span)
+      def liftK: F ~> Kleisli[F, Span[F], *] = Kleisli.liftK
     }
 }
