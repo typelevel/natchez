02f789ef8b3821ce1f2e5e3296b72b73d84327ae
Arman Bilge <armanbilge@gmail.com>
2021-11-17 17:58:31 -0500

    Scalacheck prop for XRay header (#1)

--- modules/xray/shared/src/test/scala/XRaySuite.scala
+++ modules/xray/shared/src/test/scala/XRaySuite.scala
@@ -5,9 +5,12 @@
 package natchez
 package xray
 
-import munit.FunSuite
+import munit.ScalaCheckSuite
+import org.scalacheck.Arbitrary
+import org.scalacheck.Gen
+import org.scalacheck.Prop._
 
-class XRaySuite extends FunSuite {
+class XRaySuite extends ScalaCheckSuite {
 
   val rootId = "1-5759e988-bd862e3fe1be46a994272793"
   val parentId = "53995c3f42cd8ad8"
@@ -16,6 +19,21 @@ class XRaySuite extends FunSuite {
   val parent =
     "Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8;Sampled=1"
   val notSampled = "Root=1-5759e988-bd862e3fe1be46a994272793;Sampled=0"
+
+  val genTraceId = for {
+    time <- Gen.buildableOfN[String, Char](8, Gen.hexChar)
+    identifier <- Gen.buildableOfN[String, Char](24, Gen.hexChar)
+  } yield s"1-$time-$identifier"
+
+  val genSegmentId = Gen.buildableOfN[String, Char](16, Gen.hexChar)
+
+  val genHeader = for {
+    traceId <- genTraceId
+    parentId <- Gen.option(genSegmentId)
+    sampled <- Gen.prob(0.5)
+  } yield XRaySpan.XRayHeader(traceId, parentId, sampled)
+
+  implicit val arbitraryHeader: Arbitrary[XRaySpan.XRayHeader] = Arbitrary(genHeader)
 
   test("header parsing") {
 
@@ -41,6 +59,14 @@ class XRaySuite extends FunSuite {
     assertEquals(XRaySpan.encodeHeader(rootId, None, true), noParent)
     assertEquals(XRaySpan.encodeHeader(rootId, Some(parentId), true), parent)
     assertEquals(XRaySpan.encodeHeader(rootId, None, false), notSampled)
+  }
+
+  property("header encoding/parsing round-trip") {
+    forAll { (header: XRaySpan.XRayHeader) =>
+      val encoded = XRaySpan.encodeHeader(header.traceId, header.parentId, header.sampled)
+      val parsed = XRaySpan.parseHeader(encoded)
+      assertEquals(parsed, Some(header))
+    }
   }
 
 }
