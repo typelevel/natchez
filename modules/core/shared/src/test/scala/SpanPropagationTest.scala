2ed398fc9a82abf4682219a50f2d743d1c46fec0
Arman Bilge <armanbilge@gmail.com>
2022-12-14 14:10:38 +0000

    Make the test more interesting

--- modules/core/shared/src/test/scala/SpanPropagationTest.scala
+++ modules/core/shared/src/test/scala/SpanPropagationTest.scala
@@ -32,13 +32,16 @@ class SpanPropagationTest extends InMemorySuite {
     new TraceTest {
       def program[F[_]: MonadCancelThrow: Trace] =
         Trace[F].spanR("parent").use { f =>
-          Trace[F].span("child")(f(MonadCancelThrow[F].unit) *> Trace[F].put("answer" -> 42))
+          Trace[F].span("child")(
+            f(Trace[F].put("question" -> "ultimate")) *> Trace[F].put("answer" -> 42)
+          )
         }
 
       def expectedHistory = List(
         (Lineage.Root, NatchezCommand.CreateRootSpan("root", Kernel(Map()))),
         (Lineage.Root, NatchezCommand.CreateSpan("parent", None)),
         (Lineage.Root, NatchezCommand.CreateSpan("child", None)),
+        (Lineage.Root / "parent", NatchezCommand.Put(List("question" -> "ultimate"))),
         (Lineage.Root / "child", NatchezCommand.Put(List("answer" -> 42))),
         (Lineage.Root, NatchezCommand.ReleaseSpan("child")),
         (Lineage.Root, NatchezCommand.ReleaseSpan("parent")),
