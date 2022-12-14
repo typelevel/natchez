a7ffa4ee433db13f9c90469ca144ddf3b4a1da77
Arman Bilge <armanbilge@gmail.com>
2022-12-14 14:04:00 +0000

    Add test for `spanR`

--- modules/core/shared/src/test/scala/SpanPropagationTest.scala
+++ modules/core/shared/src/test/scala/SpanPropagationTest.scala
@@ -4,12 +4,15 @@
 
 package natchez
 
+import cats.effect.MonadCancelThrow
+import cats.syntax.all._
+
 class SpanPropagationTest extends InMemorySuite {
 
   traceTest(
     "propagation",
     new TraceTest {
-      def program[F[_]: Trace] =
+      def program[F[_]: MonadCancelThrow: Trace] =
         Trace[F].span("parent")(Trace[F].span("child")(Trace[F].put("answer" -> 42)))
 
       def expectedHistory = List(
@@ -18,6 +21,26 @@ class SpanPropagationTest extends InMemorySuite {
         (Lineage.Root / "parent", NatchezCommand.CreateSpan("child", None)),
         (Lineage.Root / "parent" / "child", NatchezCommand.Put(List("answer" -> 42))),
         (Lineage.Root / "parent", NatchezCommand.ReleaseSpan("child")),
+        (Lineage.Root, NatchezCommand.ReleaseSpan("parent")),
+        (Lineage.Root, NatchezCommand.ReleaseRootSpan("root"))
+      )
+    }
+  )
+
+  traceTest(
+    "spanR",
+    new TraceTest {
+      def program[F[_]: MonadCancelThrow: Trace] =
+        Trace[F].spanR("parent").use { f =>
+          Trace[F].span("child")(f(MonadCancelThrow[F].unit) *> Trace[F].put("answer" -> 42))
+        }
+
+      def expectedHistory = List(
+        (Lineage.Root, NatchezCommand.CreateRootSpan("root", Kernel(Map()))),
+        (Lineage.Root, NatchezCommand.CreateSpan("parent", None)),
+        (Lineage.Root, NatchezCommand.CreateSpan("child", None)),
+        (Lineage.Root / "child", NatchezCommand.Put(List("answer" -> 42))),
+        (Lineage.Root, NatchezCommand.ReleaseSpan("child")),
         (Lineage.Root, NatchezCommand.ReleaseSpan("parent")),
         (Lineage.Root, NatchezCommand.ReleaseRootSpan("root"))
       )
