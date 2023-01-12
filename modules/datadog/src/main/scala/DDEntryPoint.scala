3e0d11b4798bee65df213b1ff62db1a683c8f426
Brian Holt <bholt@dwolla.com>
2023-01-12 13:30:01 -0600

    add links and span kind to DataDog

--- modules/datadog/src/main/scala/DDEntryPoint.scala
+++ modules/datadog/src/main/scala/DDEntryPoint.scala
@@ -9,32 +9,47 @@ import cats.effect._
 import cats.syntax.all._
 import io.opentracing.propagation.{Format, TextMapAdapter}
 import io.{opentracing => ot}
+import natchez.datadog.DDTracer._
 
 import java.net.URI
 
 final class DDEntryPoint[F[_]: Sync](tracer: ot.Tracer, uriPrefix: Option[URI])
     extends EntryPoint[F] {
-  override def root(name: String): Resource[F, Span[F]] =
+  override def root(name: String, options: Span.Options): Resource[F, Span[F]] =
     Resource
-      .make(Sync[F].delay(tracer.buildSpan(name).start()))(s => Sync[F].delay(s.finish()))
-      .map(DDSpan(tracer, _, uriPrefix, Span.Options.SpanCreationPolicy.Default))
+      .make {
+        Sync[F]
+          .delay(tracer.buildSpan(name))
+          .flatTap(addSpanKind(_, options.spanKind))
+          .flatMap(options.links.foldM(_)(addLink[F](tracer)))
+          .flatMap(builder => Sync[F].delay(builder.start()))
+      }(s => Sync[F].delay(s.finish()))
+      .map(DDSpan(tracer, _, uriPrefix, options))
 
-  override def continue(name: String, kernel: Kernel): Resource[F, Span[F]] =
+  override def continue(name: String, kernel: Kernel, options: Span.Options): Resource[F, Span[F]] =
     Resource
-      .make(
-        Sync[F].delay {
-          val spanContext = tracer.extract(
-            Format.Builtin.HTTP_HEADERS,
-            new TextMapAdapter(kernel.toJava)
-          )
-          tracer.buildSpan(name).asChildOf(spanContext).start()
-        }
-      )(s => Sync[F].delay(s.finish()))
-      .map(DDSpan(tracer, _, uriPrefix, Span.Options.SpanCreationPolicy.Default))
+      .make {
+        Sync[F]
+          .delay {
+            val spanContext = tracer.extract(
+              Format.Builtin.HTTP_HEADERS,
+              new TextMapAdapter(kernel.toJava)
+            )
+            tracer.buildSpan(name).asChildOf(spanContext)
+          }
+          .flatTap(addSpanKind(_, options.spanKind))
+          .flatMap(options.links.foldM(_)(addLink[F](tracer)))
+          .flatMap(builder => Sync[F].delay(builder.start()))
+      }(s => Sync[F].delay(s.finish()))
+      .map(DDSpan(tracer, _, uriPrefix, options))
 
-  override def continueOrElseRoot(name: String, kernel: Kernel): Resource[F, Span[F]] =
-    continue(name, kernel).flatMap {
-      case null => root(name) // hurr, means headers are incomplete or invalid
+  override def continueOrElseRoot(
+      name: String,
+      kernel: Kernel,
+      options: Span.Options
+  ): Resource[F, Span[F]] =
+    continue(name, kernel, options).flatMap {
+      case null => root(name, options) // hurr, means headers are incomplete or invalid
       case span => span.pure[Resource[F, *]]
     }
 }
