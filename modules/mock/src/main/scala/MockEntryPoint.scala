63ce4093de3d35f2a30bd61668343a792f951768
Scala Steward <scala_steward@virtuslab.com>
2023-01-20 00:31:20 +0000

    Reformat with scalafmt 3.7.0
    Executed command: scalafmt --non-interactive

--- modules/mock/src/main/scala/MockEntryPoint.scala
+++ modules/mock/src/main/scala/MockEntryPoint.scala
@@ -7,47 +7,42 @@ package mock
 
 import scala.jdk.CollectionConverters._
 
-import cats.effect.{ Resource, Sync }
+import cats.effect.{Resource, Sync}
 import io.opentracing.mock.MockTracer
-import io.opentracing.propagation.{ Format, TextMapAdapter }
+import io.opentracing.propagation.{Format, TextMapAdapter}
 
-final case class MockEntrypoint[F[_] : Sync]() extends EntryPoint[F] {
+final case class MockEntrypoint[F[_]: Sync]() extends EntryPoint[F] {
 
   val mockTracer = new MockTracer()
 
-  override def root(name: String): Resource[F, Span[F]] = {
+  override def root(name: String): Resource[F, Span[F]] =
     Resource
-      .make(Sync[F].delay(mockTracer.buildSpan(name).start()))(
-        span => Sync[F].delay(span.finish())
-        )
+      .make(Sync[F].delay(mockTracer.buildSpan(name).start()))(span => Sync[F].delay(span.finish()))
       .map(MockSpan(mockTracer, _))
-  }
 
   override def continue(
-    name: String,
-    kernel: Kernel
-  ): Resource[F, Span[F]] = {
+      name: String,
+      kernel: Kernel
+  ): Resource[F, Span[F]] =
     Resource
       .make(
         Sync[F].delay {
           val spanCtxt = mockTracer.extract(
             Format.Builtin.HTTP_HEADERS,
             new TextMapAdapter(kernel.toHeaders.asJava)
-            )
+          )
           mockTracer.buildSpan(name).asChildOf(spanCtxt).start()
         }
-        )(span => Sync[F].delay(span.finish()))
+      )(span => Sync[F].delay(span.finish()))
       .map(MockSpan(mockTracer, _))
-  }
 
   override def continueOrElseRoot(
-    name: String,
-    kernel: Kernel
-  ): Resource[F, Span[F]] = {
+      name: String,
+      kernel: Kernel
+  ): Resource[F, Span[F]] =
     continue(name, kernel).flatMap {
       case null =>
         root(name)
       case span => Resource.pure[F, Span[F]](span)
     }
-  }
 }
